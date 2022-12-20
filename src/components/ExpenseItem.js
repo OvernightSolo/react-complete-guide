e079d38f34fb77462cceb6a06dbe2a0d21124215
OvernightSolo <sterlingtx75@gmail.com>
2022-12-20 15:32:39 -0600

    added dynamic data to expense items component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = props => {
   return (
     <div className="expense-item">
-      <div>December 20, 2021</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
